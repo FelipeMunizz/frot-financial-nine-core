5aa562da0bf26443d08904afbaaeddb65d14d93a
Felipe Muniz <lipe.baterra@gmail.com>
2023-05-13 13:23:33 -0300

    Novas implementações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments.ts
@@ -0,0 +1,4 @@
+export const environments = {
+    production : false,
+    endpoint : 'https://localhost:5232/api'
+}

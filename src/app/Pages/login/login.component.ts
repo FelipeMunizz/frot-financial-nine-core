25af7373ef2337a87f612ffc05f0900b14b64611
Felipe Muniz <lipe.baterra@gmail.com>
2023-05-15 20:04:09 -0300

    nova implementacoes

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(public formBuilder: FormBuilder, private router: Router) {
 
+  }
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required]]
+      }
+    )
+  }
+
+  get dadosForm(){
+    return this, this.loginForm.controls
+  }
+
+  loginUser(){
+    alert("Ok")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Pages/login/login.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { LoginComponent } from './Pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

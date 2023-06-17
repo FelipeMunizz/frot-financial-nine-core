6e50547efde9e91b7c12189f351146d34e76d0b6
Felipe Muniz <lipe.baterra@gmail.com>
2023-06-17 18:11:48 -0300

    Novas Implementações

--- src/app/Pages/dashboard/dashboard.module.ts
+++ src/app/Pages/dashboard/dashboard.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard.component";
 import { CommonModule } from "@angular/common";
 import { DasboardRoutingModule } from "./dashboard-routing.module";
+import { NavbarModule } from "src/app/Components/navbar/navbar.module";
+import { SidebarModule } from "src/app/Components/sidebar/sidebar.module";
 
 @NgModule({
     providers: [],
     declarations: [DashboardComponent],
-    imports: [CommonModule, DasboardRoutingModule]
+    imports: 
+    [
+        CommonModule, 
+        DasboardRoutingModule, 
+        NavbarModule, 
+        SidebarModule
+    ]
 })
 
 export class DashboardModule{}

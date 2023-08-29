4d7abda3e82aea1e59fbfed693172d91c9f5cf25
Felipe Muniz <lipe.baterra@gmail.com>
2023-08-28 22:53:31 -0300

    Implementação servide Sistema Financeiro

--- src/app/Services/sistema.service.ts
+++ src/app/Services/sistema.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { environments } from "src/environments";
+import { SistemaFinanceiro } from "../Models/SistemaFinanceiro";
 
 @Injectable({
     providedIn: 'root'
@@ -9,5 +10,19 @@ import { environments } from "src/environments";
 export class SistemaService{
     constructor(private httpClient : HttpClient){}
 
-    private readonly baseUrl = environments["endpoint"]
+    private readonly baseUrl = environments["endpoint"];
+
+    AdicionarSistemaFinanceiro(sistemaFinanceiro: SistemaFinanceiro){
+        return this.httpClient.post<SistemaFinanceiro>(`${this.baseUrl}/adicionarsistemafinanceiro`,
+        sistemaFinanceiro)
+    }
+
+    ListaSistemaUsuario(email: string){
+        return this.httpClient.get(`${this.baseUrl}/listasistemausuario?email=${email}`)
+    }
+
+    CadastrarUsuarioSistemaFinanceiro(idSistema:number, email:string){
+        return this.httpClient.post(`${this.baseUrl}/cadastrarusuariosistemafinanceiro?idSistema=${idSistema}`, 
+        email)
+    }
 }
